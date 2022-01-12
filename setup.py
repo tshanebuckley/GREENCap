24e149d071fbcafbc92c386bacf2639b87cc3616
tshanebuckley <42987118+tshanebuckley@users.noreply.github.com>
2022-01-11 21:23:48 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='greencap',
+      version='0.1',
+      description='GREENCap: Asynchronous requests and middleware API for REDCap.',
+      url='https://github.com/tshanebuckley/GREENCap',
+      author='Maintainer: Shane Buckley',
+      author_email='buckleyts3@email.unc.edu',
+      #license='MIT',
+      python_requires='>=3.6',
+      #package_dir={'': 'GREENCap/greencap'},
+      packages=['greencap'],
+      include_package_data=True,
+      #packages=find_packages(),
+      install_requires=[],
+      zip_safe=False
+      )
