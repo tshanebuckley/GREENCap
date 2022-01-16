bf8c123682958a864f3f1d7809ac21bac3a71cbe
tshanebuckley <42987118+tshanebuckley@users.noreply.github.com>
2022-01-15 22:42:15 -0500

    Update error.py

--- greencap/error.py
+++ greencap/error.py
@@ -1,6 +1,9 @@
 # redcap connection error
 class REDCapConnectError(Exception):
-    def __init__(self, name:str, message:str) -> None:
+    def __init__(self, name:str, message:str="Unable to connect to REDCap project {name}") -> None:
         self.name = name
         self. message = message
         super().__init__(message)
+
+    def __str__(self):
+        return self.message.format(name=self.name)
