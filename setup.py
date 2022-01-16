61654e5fca59453281e07d290bdbc1dc05f660fc
tshanebuckley <42987118+tshanebuckley@users.noreply.github.com>
2022-01-16 18:56:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,12 +34,12 @@ setup(name='greencap',
             "dtale"
       ],
       data_files=[
-            ('/{HOME}/.greencap'.format(HOME=USER_PATH), ['greencap_config.yaml']),
-            ('/{HOME}/.greencap/projects'.format(HOME=USER_PATH), []),
-            ('/{HOME}/.greencap/users'.format(HOME=USER_PATH), []),
-            ('/{HOME}/.greencap/groups'.format(HOME=USER_PATH), []),
-            ('/{HOME}/.greencap/shiny'.format(HOME=USER_PATH), ['open_dtale.py', 'redcap_webapp.R']),
-            ('/{HOME}/.greencap/api'.format(HOME=USER_PATH), ['api.py'])
+            ('{HOME}/.greencap'.format(HOME=USER_PATH), ['greencap_config.yaml']),
+            ('{HOME}/.greencap/projects'.format(HOME=USER_PATH), []),
+            ('{HOME}/.greencap/users'.format(HOME=USER_PATH), []),
+            ('{HOME}/.greencap/groups'.format(HOME=USER_PATH), []),
+            ('{HOME}/.greencap/shiny'.format(HOME=USER_PATH), ['open_dtale.py', 'redcap_webapp.R']),
+            ('{HOME}/.greencap/api'.format(HOME=USER_PATH), ['api.py'])
       ],
       zip_safe=False
       )
