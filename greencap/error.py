87a2155b59fb3c600ff1765d601dca73f27d1cdb
tshanebuckley <42987118+tshanebuckley@users.noreply.github.com>
2022-01-15 23:10:37 -0500

    Update error.py

--- greencap/error.py
+++ greencap/error.py
@@ -7,3 +7,13 @@ class REDCapConnectError(Exception):
 
     def __str__(self):
         return self.message.format(name=self.name)
+
+# redcap object error
+class REDCapObjectError(Exception):
+    def __init__(self, name:str, message:str="REDCap object model incorrect for project {name}") -> None:
+        self.name = name
+        self. message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return self.message.format(name=self.name)
